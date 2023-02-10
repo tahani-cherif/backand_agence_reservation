131f622aba1460de9ad12b7996869fc73fc5cd72
@tahani-cherif <tahani.cherif@avec.fr>
2023-02-10 12:16:04 +0100

    update client

--- routes/clientroute.js
+++ routes/clientroute.js
@@ -6,5 +6,6 @@ router.post('/postclient',clientcontroller.postclient)
 router.get('/getclient/:id',clientcontroller.getclient)
 router.get('/getclientbyemail',clientcontroller.getclientbymail)
 router.delete('/deletclient/:id',clientcontroller.deletclient)
+router.put('/updateclient/:id',clientcontroller.updateclient)
 
 module.exports=router
