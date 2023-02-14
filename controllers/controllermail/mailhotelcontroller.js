f4d0e6a7dde7a781538c1a0c67ab895355ef345c
tahani-cherif <cheriftahani92@gmail.com>
2023-02-14 22:48:01 +0100

    fix:email

--- controllers/controllermail/mailhotelcontroller.js
+++ controllers/controllermail/mailhotelcontroller.js
@@ -21,15 +21,21 @@ const transporter = nodemailer.createTransport({
         error: 'You must provide a body to create',
       });
     }
+
+    let desc=`<div>
+    <h3>Reservation de agence ${body.nom_agence}</h3>
+    <p>agance ${body.nom_agence} reserver ${body.nb_chambre} chambre ${body.type} a ${body.nb_client} clients </p>
+    `
+    if(parseInt(body.enfant_gratuit)===1)
+    {
+      desc+=`<p>avec un enfant bebe -2ans</p>`
+    }
+    desc+=`<p>date de reservation : ${body.date_debut} --> ${body.date_fin}</p>
+    </div>`
       const mailOptionsTransmissions = {
         from: 'dzagence.responsable@gmail.com',
         to:body.email,
-        html: ` <div>
-                   <h3>Reservation de agence ${body.nom_agence}</h3>
-                   <p>agance ${body.nom_agence} reserver ${body.nb_chambre} chambre ${body.type} a ${body.nb_client} clients</p>
-                   <p>date de reservation : ${body.date_debut} --> ${body.date_fin}</p>
-
-                </div>`,
+        html: desc,
         subject: 'Reservation chambre dans Hotel'
       };
       transporter.sendMail(
